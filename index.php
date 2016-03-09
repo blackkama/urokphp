822581b719e1f4699f8e714c3df1fd0e8bf0cdb5
as7bzx92dd <g04032016@mail.ru>
2016-03-09 14:55:43 +0300

    Новое изменение
    Посмотрите

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
 	<title>Document</title>
 </head>
 <body>
+<p>Сложение двух чисел</p>
+<form action="index.php" name="myform" method="post"> 
+       <input type="text" name="chisloa" size="5" maxlength="5" value="Число a">
+       <input type="text" name="chisloa" size="5" maxlength="5" value="Число b">
+<input name="Submit" type=submit value="Сложить"> 
+</form>
 	<?php
-	$a=2;
-	$b=4;
+	$a= $_POST['chisloa']; 
+	echo $a;
+	$b=$_POST['chislob'];
+	echo $b;
 	$sum=$a+$b;
 	echo $sum;
 	?>
