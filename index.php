83d8e7d1ba542c34452dbe738c9e14b4491ea82d
as7bzx92dd <g04032016@mail.ru>
2016-03-09 14:49:46 +0300

    ЛабаPHP
    php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	$a=2;
+	$b=4;
+	$sum=$a+$b;
+	echo $sum;
+	?>
+</body>
+</html>
